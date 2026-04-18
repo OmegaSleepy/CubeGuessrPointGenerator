fae44615bb727d0827380ea641226041baeb1f40
martin <martingeorgiev2307@gmail.com>
2026-04-19 01:04:38 +0300

    fix: better null handling in ChunkExtract

--- src/main/java/org/omega/logic/ChunkExtract.java
+++ src/main/java/org/omega/logic/ChunkExtract.java
@@ -4,11 +4,13 @@ import de.pauleff.jmcx.api.IChunk;
 import de.pauleff.jnbt.core.Tag_Compound;
 import org.omega.util.Package;
 import org.omega.value.Chunk;
+import org.omega.value.Heightmaps;
 
 import java.io.IOException;
 import java.util.List;
 
 public class ChunkExtract {
+
     public static Chunk extract(IChunk iChunk) throws IOException, NullPointerException{
 
         List<List<String>> sectionBlockPallets = iChunk.getNBTData().getList("sections").getData().stream()
@@ -25,8 +27,9 @@ public class ChunkExtract {
                 .map(el -> el.getCompound("block_states"))
                 .map(el -> el.getLongArray("data")).toList();
 
-        long[] heightmap = iChunk.getNBTData().getCompound("Heightmaps").getLongArray("MOTION_BLOCKING_NO_LEAVES");
+        long[] heightmap = iChunk.getNBTData().getCompound("Heightmaps").getLongArray(Heightmaps.WORLD_SURFACE.name().toUpperCase());
 
+        if (heightmap==null) throw new NullPointerException("Chunk at: " + iChunk.getX() + " " + iChunk.getZ() + " has a null heightmap");
         return new Chunk(iChunk.getX(),iChunk.getZ(), Package.packageSections(sectionBlockPallets, sectionData), heightmap);
     }
 }
