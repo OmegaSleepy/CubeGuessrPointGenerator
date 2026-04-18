5f25c19d81547107f64cfa6fb0cef53298295023
martin <martingeorgiev2307@gmail.com>
2026-04-18 22:43:35 +0300

    feat: reimplemented heightmaps

--- src/main/java/org/omega/value/Chunk.java
+++ src/main/java/org/omega/value/Chunk.java
@@ -1,5 +1,7 @@
 package org.omega.value;
 
+import de.pauleff.jmcx.api.IChunk;
+
 import java.io.IOException;
 
 public record Chunk(int chunkX, int chunkZ, Section[] sections, long[] heightmap) {
@@ -11,7 +13,7 @@ public record Chunk(int chunkX, int chunkZ, Section[] sections, long[] heightmap
         return "X " + chunkX + " Y " + chunkZ;
     }
 
-    public String getBlock(int x, int y, int z) {
+    public String getBlock (int x, int y, int z) {
         int normalizedY = y + 64;
 
         int sectionIndex = normalizedY >> 4;
@@ -20,7 +22,33 @@ public record Chunk(int chunkX, int chunkZ, Section[] sections, long[] heightmap
 
         Section section = sections[sectionIndex];
 
-        // 3. Use normalizedY for the relative coordinate (y & 15)
         return (section == null) ? "minecraft:air" : section.getBlock(x & 15, normalizedY & 15, z & 15);
     }
+
+    public int getHeightmap (int x, int z) {
+        int index = (x << 4) | z;
+        int bitsPerEntry = 9;
+
+        int bitsPerLong = 64 / bitsPerEntry;
+        int longIndex = index / bitsPerLong;
+        int bitOffset = (index % bitsPerLong) * bitsPerEntry;
+
+        long mask = (1L << bitsPerEntry) - 1;
+
+        return (int) ((heightmap[longIndex] >>> bitOffset) & mask);
+    }
+
+    public static long[] getHeightmap (IChunk iChunk) throws IOException {
+        if (iChunk == null || iChunk.getNBTData() == null) return null;
+
+        var heightmaps = iChunk.getNBTData().getCompound("Heightmaps");
+        if (heightmaps == null) return null;
+
+        long[] data = heightmaps.getLongArray("MOTION_BLOCKING_NO_LEAVES");
+        if (data == null || data.length == 0) return null;
+
+        return data;
+
+    }
+
 }
