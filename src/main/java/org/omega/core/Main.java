f56bd201b4b154ad949967b23938f283a60f754c
martin <martingeorgiev2307@gmail.com>
2026-04-18 22:53:08 +0300

    feat: all regions as a List<PointXZ> in Main

--- src/main/java/org/omega/core/Main.java
+++ src/main/java/org/omega/core/Main.java
@@ -1,22 +1,36 @@
 package org.omega.core;
 
-import de.pauleff.jmcx.formats.anvil.AnvilReader;
-import org.omega.logic.ChunkExtract;
-import org.omega.util.Clean;
-import org.omega.util.statistic.StringStats;
-import org.omega.value.Chunk;
-import org.omega.value.Section;
+import org.omega.core.algorithms.SimpleAlgorithm;
+import org.omega.value.PointXYZ;
+import org.omega.value.PointXZ;
 
 import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.*;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.Executors;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
     public static final File world = new File("/home/martin/Documents/old world/s1/world/region");
+
+    public static final List<PointXZ> regionCoordinates = new ArrayList<>();
+
+    private static final Pattern pattern = Pattern.compile("r\\.(-?\\d+)\\.(-?\\d+)\\.mca");
+
+    static {
+        Arrays.stream(Objects.requireNonNull(world.listFiles())).map(File::getName).forEach(fileName -> {
+            Matcher matcher = pattern.matcher(fileName);
+
+            if (matcher.matches()) {
+                int x = Integer.parseInt(matcher.group(1));
+                int z = Integer.parseInt(matcher.group(2));
+
+                 regionCoordinates.add(new PointXZ(x, z));
+            }
+        });
+    }
 
 
 }
