a645392ca6b08333fe3e259c7a079c4f61562d59
martin <martingeorgiev2307@gmail.com>
2026-04-18 20:25:36 +0300

    docs: Point.java

--- src/main/java/org/omega/value/Point.java
+++ src/main/java/org/omega/value/Point.java
@@ -1,11 +1,13 @@
 package org.omega.value;
 
+///A point in 3D space
 public record Point(int x, int y, int z) {
 
     public String command() {
         return "/tp @s %d %d %d".formatted(x, y, z);
     }
 
+    ///Calculate distance between two points in 3D
     public double distanceXYZ(Point other) {
         long dx = (long) other.x - x;
         long dy = (long) other.y - y;
@@ -13,6 +15,7 @@ public record Point(int x, int y, int z) {
         return dx * dx + dy * dy + dz * dz;
     }
 
+    ///Returns `true` if distance is more than minDistance
     public boolean proximityCheckXYZ(Point other, double minDistance) {
         long dx = (long) other.x - x;
         long dy = (long) other.y - y;
