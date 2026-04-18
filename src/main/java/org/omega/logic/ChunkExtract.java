a7905ff51cec055682e15d98fc1e660f1bf3622f
martin <martingeorgiev2307@gmail.com>
2026-04-19 01:29:27 +0300

    feat: autoremove chunks that have biomes that are an ocean

--- src/main/java/org/omega/logic/ChunkExtract.java
+++ src/main/java/org/omega/logic/ChunkExtract.java
@@ -1,13 +1,18 @@
 package org.omega.logic;
 
 import de.pauleff.jmcx.api.IChunk;
+import de.pauleff.jnbt.core.Tag;
 import de.pauleff.jnbt.core.Tag_Compound;
+import de.pauleff.jnbt.core.Tag_String;
 import org.omega.util.Package;
 import org.omega.value.Chunk;
 import org.omega.value.Heightmaps;
 
 import java.io.IOException;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class ChunkExtract {
 
@@ -26,6 +31,24 @@ public class ChunkExtract {
                 .map(el -> (Tag_Compound) el)
                 .map(el -> el.getCompound("block_states"))
                 .map(el -> el.getLongArray("data")).toList();
+
+
+        Set<String> biomes = new HashSet<>();
+
+        iChunk.getNBTData().getList("sections").getData().stream()
+                .map(el -> (Tag_Compound) el)
+                .map(el -> el.getCompound("biomes"))
+                .map(el -> el.getList("palette").getData())
+                .map(list -> list.stream()
+                        .map(el -> (Tag_String) el)
+                        .map(Tag::getData).collect(Collectors.toSet())
+                ).forEach(biomes::addAll);
+
+        for (String biome : biomes) {
+            if (biome.contains("ocean")) return null;
+            if (biome.contains("river")) return null;
+        }
+
 
         long[] heightmap = iChunk.getNBTData().getCompound("Heightmaps").getLongArray(Heightmaps.WORLD_SURFACE.name().toUpperCase());
 
