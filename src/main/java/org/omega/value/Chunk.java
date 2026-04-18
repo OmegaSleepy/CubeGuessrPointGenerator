bed8c17bf41522c6477e5eacae102a43f99fb2e1
martin <martingeorgiev2307@gmail.com>
2026-04-18 21:16:34 +0300

    feat!: Chunk OOP change - class to record

--- src/main/java/org/omega/value/Chunk.java
+++ src/main/java/org/omega/value/Chunk.java
@@ -1,39 +1,15 @@
 package org.omega.value;
 
-public class Chunk {
-    private final int chunkX;
-    private final int chunkZ;
-    private final Section[] sections;
-
-    public Chunk(int x, int z, int sectionCount) {
-        this.chunkX = x;
-        this.chunkZ = z;
-        this.sections = new Section[sectionCount];
+public record Chunk(int chunkX, int chunkZ, Section[] sections) {
+    public Chunk (int x, int z, int sectionCount) {
+        this(x, z, new Section[sectionCount]);
     }
 
-    public Chunk(int x, int z, Section[] sections) {
-        this.chunkX = x;
-        this.chunkZ = z;
-        this.sections = sections;
+    public String getChunkCoordinates () {
+        return "X " + chunkX + " Y " + chunkZ;
     }
 
-    public int getChunkX () {
-        return chunkX;
-    }
-
-    public int getChunkZ () {
-        return chunkZ;
-    }
-
-    public Section[] getSections () {
-        return sections;
-    }
-
-    public String getChunkCoordinates(){
-        return "X: " + chunkX + " Y: " + chunkZ;
-    }
-
-    public String getBlock(int x, int y, int z) {
+    public String getBlock (int x, int y, int z) {
         int sectionIndex = y >> 4; // Same as y / 16
         if (sectionIndex < 0 || sectionIndex >= sections.length) return "minecraft:air";
 
