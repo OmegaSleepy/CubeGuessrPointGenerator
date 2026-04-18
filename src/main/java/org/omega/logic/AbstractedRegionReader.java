c4e215503efdc53989403eed14749c37a590d72e
martin <martingeorgiev2307@gmail.com>
2026-04-18 20:42:55 +0300

    feat: AbstractedRegionReader for returning all IChunks/Chunks from a mcafile

---
+++ src/main/java/org/omega/logic/AbstractedRegionReader.java
@@ -0,0 +1,34 @@
+package org.omega.logic;
+
+import de.pauleff.jmcx.api.IChunk;
+import de.pauleff.jmcx.formats.anvil.AnvilReader;
+import org.omega.util.Clean;
+import org.omega.value.Chunk;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+public class AbstractedRegionReader {
+    public static List<IChunk> read(File mcaFile) throws IOException {
+
+        try (AnvilReader reader = new AnvilReader(mcaFile)) {
+            var region = reader.readRegion();
+            return Clean.cleanNullChunks(region.getChunks());
+        } catch (Exception e) {
+            throw e;
+        }
+    }
+
+    public static List<Chunk> readAndFormat(File mcaFile) throws IOException {
+        List<IChunk> iChunks = read(mcaFile);
+        return iChunks.stream().map(iChunk -> {
+                    try {
+                        return ChunkExtract.extract(iChunk);
+                    } catch (Exception e) {
+                        return null;
+                    }
+                }
+        ).toList();
+    }
+}
