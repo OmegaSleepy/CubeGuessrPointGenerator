21abdef15f519ca522b7b6f5563236e1923cdaa9
martin <martingeorgiev2307@gmail.com>
2026-04-19 01:14:36 +0300

    feat: more region util

--- src/main/java/org/omega/value/Region.java
+++ src/main/java/org/omega/value/Region.java
@@ -9,15 +9,17 @@ import java.util.List;
 
 public class Region {
     private final Chunk[][] chunks = new Chunk[32][32];
+    private final List<PointXZ> chunkHeadersList;
     public final int x;
     public final int z;
 
-    public Region(int x, int z, File mcaFile) throws IOException {
+    public Region (int x, int z, File mcaFile) throws IOException {
         this.x = x;
         this.z = z;
 
         List<Chunk> chunkList = AbstractedRegionReader.readAndFormat(mcaFile);
         chunkList = Clean.cleanNullChunks(chunkList);
+        this.chunkHeadersList = chunkList.stream().map(Chunk::getChunkCoordinates).toList();
 
         for (Chunk chunk : chunkList) {
             int localX = getLocal(chunk.chunkX());
@@ -27,12 +29,27 @@ public class Region {
         }
     }
 
-    private int getLocal(int coord) {
+    public static PointXZ getRegionPointXZFromGlobalBlockXZ (PointXZ currentPoint) {
+        int localX = currentPoint.x() / 512;
+        int localZ = currentPoint.z() / 512;
+
+        return new PointXZ(localX, localZ);
+    }
+
+    private int getLocal (int coord) {
         return ((coord % 32) + 32) % 32;
     }
 
-    public Chunk getChunk(int localX, int localZ) {
+    public Chunk getChunk (int localX, int localZ) {
         return chunks[localX][localZ];
+    }
+
+    public Chunk getChunk (PointXZ globalXZ) {
+        return getChunk(getLocal(globalXZ.x()), getLocal(globalXZ.z()));
+    }
+
+    public List<PointXZ> getChunkHeadersList () {
+        return chunkHeadersList;
     }
 
     public Chunk[][] getChunks () {
