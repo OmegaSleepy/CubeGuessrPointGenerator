79608d80cf3e708ab47a7e22c6aaad96f941f209
martin <martingeorgiev2307@gmail.com>
2026-04-18 22:29:10 +0300

    feat: clean Main.main

--- src/main/java/org/omega/core/Main.java
+++ src/main/java/org/omega/core/Main.java
@@ -1,21 +1,22 @@
 package org.omega.core;
 
 import de.pauleff.jmcx.formats.anvil.AnvilReader;
+import org.omega.logic.ChunkExtract;
+import org.omega.util.Clean;
+import org.omega.util.statistic.StringStats;
+import org.omega.value.Chunk;
+import org.omega.value.Section;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.Executors;
 
 public class Main {
     public static final File world = new File("/home/martin/Documents/old world/s1/world/region");
 
-    static void main () {
-        try (AnvilReader reader = new AnvilReader(new File(world.getAbsoluteFile()+"/r.-1.4.mca"))){
 
-            var region = reader.readRegion();
-            var chunks = region.getChunks();
-
-        } catch (IOException e){
-
-        }
-    }
 }
