25f30b49a4e13f269488dddeaa02a757a5b8617d
martin <martingeorgiev2307@gmail.com>
2026-04-19 12:08:56 +0300

    feat: performance optimizations in SimpleAlgorithm - only search through chunks that exits and are within the centers Massive performance gains

--- src/main/java/org/omega/core/algorithms/SimpleAlgorithm.java
+++ src/main/java/org/omega/core/algorithms/SimpleAlgorithm.java
@@ -1,66 +1,66 @@
 package org.omega.core.algorithms;
 
-import org.omega.core.Main;
 import org.omega.logic.IPointAlgorithm;
 import org.omega.util.statistic.RandomPoint;
 import org.omega.value.*;
 
 import java.io.IOException;
 import java.util.*;
+import java.util.logging.Logger;
 
 public class SimpleAlgorithm implements IPointAlgorithm {
 
+    private final Logger logger = Logger.getLogger("SimpleAlgorithm");
+
     private final List<PointXYZ> pointXYZList = new ArrayList<>();
-    private final int MIN_DISTANCE = 25;
-    private final int MAX_DISTANCE = 800;
+    private final int MIN_DISTANCE = 10;
 
     private final Random random = new Random();
 
-    private final List<PointXYZ> centers = List.of(new PointXYZ(650, 80, 560));
+    private final List<CircleCenter> centers = List.of(
+            new CircleCenter(new PointXYZ(650, 80, 560), 800));
 
-    private Set<PointXZ> uniqueRegionCoords = new HashSet<>();
+    private final List<PointXZ> validChunkCoords;
 
-    public SimpleAlgorithm(){
-        Set<PointXZ> uniqueRegionCoords = new HashSet<>();
+    public SimpleAlgorithm () {
+        Set<PointXZ> chunksInRadius = new HashSet<>();
 
-        for (PointXYZ center : centers) {
-            int minX = center.x() - MAX_DISTANCE;
-            int maxX = center.x() + MAX_DISTANCE;
-            int minZ = center.z() - MAX_DISTANCE;
-            int maxZ = center.z() + MAX_DISTANCE;
+        for (CircleCenter circleCenter : centers) {
+            var center = circleCenter.pointXYZ();
+            int radius = circleCenter.radius();
+            PointXZ centerXZ = new PointXZ(center.x(), center.z());
 
-            for (int x = minX; x <= maxX + 512; x += 512) {
-                for (int z = minZ; z <= maxZ + 512; z += 512) {
-                    PointXZ currentPoint = new PointXZ(Math.min(x, maxX), Math.min(z, maxZ));
-                    PointXZ regionCoord = Region.getRegionPointXZFromGlobalBlockXZ(currentPoint);
+            int minX = center.x() - radius;
+            int maxX = center.x() + radius;
+            int minZ = center.z() - radius;
+            int maxZ = center.z() + radius;
 
-                    uniqueRegionCoords.add(regionCoord);
+            for (int x = minX; x <= maxX + 16; x += 16) {
+                for (int z = minZ; z <= maxZ + 16; z += 16) {
+                    PointXZ currentBlock = new PointXZ(Math.min(x, maxX), Math.min(z, maxZ));
+                    PointXZ chunkCoord = Chunk.getChunkPointXZFromGlobalBlockXZ(currentBlock);
+
+                    if (chunkCoord.isWithinRadius(centerXZ, radius)) {
+                        if (World.chunkExits(chunkCoord)){
+                            chunksInRadius.add(chunkCoord);
+                        }
+                    }
                 }
             }
         }
-
-        this.uniqueRegionCoords = uniqueRegionCoords;
-
+        this.validChunkCoords = new ArrayList<>(chunksInRadius);
     }
 
     @Override
     public PointXYZ generatePoint () {
-        var list = new ArrayList<>(uniqueRegionCoords);
-        var randomRegionPoint = list.get(random.nextInt(list.size()));
+        var list = new ArrayList<>(validChunkCoords);
+        var randomChunkCoord = list.get(random.nextInt(list.size()));
 
         Chunk randomChunk;
         try {
-
-            Region region = World.getRegion(randomRegionPoint);
-            assert region != null;
-            var chunkList = region.getChunkHeadersList();
-            if (chunkList.isEmpty()) {
-                return null;
-            }
-            var randomChunkCoordinate = chunkList.get(random.nextInt(chunkList.size()));
-            randomChunk = region.getChunk(randomChunkCoordinate);
-
+            randomChunk = World.getChunk(randomChunkCoord);
         } catch (IOException e) {
+            logger.severe("Error reading chunk " + randomChunkCoord);
             return null;
         }
 
@@ -78,17 +78,21 @@ public class SimpleAlgorithm implements IPointAlgorithm {
             newPoint = randomChunk.getGlobalFromLocal(randomCoordinate);
 
             passes++;
-            if (passes > 10) return null;
+            if (passes > 10) {
+                logger.warning("Exhausted chunk " + Chunk.getLocalFromGlobal(randomCoordinate) + " at " + randomChunk.getChunkCoordinates());
+                return null;
+            }
         } while (isInvalidPoint(newPoint));
 
         var resultCoordinate = new PointXYZ(newPoint.x(), heightmapValue, newPoint.z());
 
         pointXYZList.add(resultCoordinate);
-        System.out.println(resultCoordinate + " GENERATED");
+        validChunkCoords.remove(randomChunkCoord);
+        logger.info(resultCoordinate + " GENERATED");
         return resultCoordinate;
     }
 
-    private boolean isInvalidPoint(PointXZ pointXZ) {
+    private boolean isInvalidPoint (PointXZ pointXZ) {
         for (PointXYZ existing : pointXYZList) {
             if (pointXZ.isWithinRadius(new PointXZ(existing.x(), existing.z()), MIN_DISTANCE)) {
                 return true;
@@ -96,8 +100,13 @@ public class SimpleAlgorithm implements IPointAlgorithm {
         }
 
         boolean nearCenter = false;
-        for (PointXYZ center : centers) {
-            if (pointXZ.isWithinRadius(new PointXZ(center.x(), center.z()), MAX_DISTANCE)) {
+
+        for (CircleCenter circleCenter : centers) {
+
+            var center = circleCenter.pointXYZ();
+            int maxDistance = circleCenter.radius();
+
+            if (pointXZ.isWithinRadius(new PointXZ(center.x(), center.z()), maxDistance)) {
                 nearCenter = true;
                 break;
             }
